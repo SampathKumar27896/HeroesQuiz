c740ff84fe98861582d718e5d6c4597003e28ebd
SampathKumar27896 <sampathkumar27896@hotmail.com>
2017-11-13 23:25:38 +0530

    Adding all files and folders

---
+++ public/main.js
@@ -0,0 +1,110 @@
+//DOM objects
+
+const view = {
+
+	score:document.querySelector('#score strong'),
+	question:document.getElementById('question'),
+	result:document.getElementById('result'),
+	info:document.getElementById('info'),
+
+	//function 
+
+	render(target,content,attributes){
+
+		for(const key in attributes){
+				target.setAttribute(key,attributes[key]);
+		}
+		target.innerHtML = content;
+	}
+
+};
+
+
+
+
+//Heroic names and real names of Heroes
+
+
+const quiz = [
+	
+		{name : "Superman" , realName : "Clark Kent"},
+		{name  : "Batman" , realName : "Wayne Bruce"},
+		{name : "WonderWomen" , realName : "Diana Prince"},
+
+];
+
+//Main game Object 
+
+const game = {
+
+	start(quiz){
+
+			this.questions = [...quiz];
+			this.score = 0;
+
+			//main game loop
+
+			for(const q of this.questions){
+				this.question = q;
+				this.ask(this.question);
+			}
+
+			this.gameOver();
+
+	},
+
+	ask(question){
+
+		const ques = `What is ${this.question.name}'s real name?.`;
+
+		//function call to render
+
+		view.render(view.question,ques);
+
+		const response = prompt(ques);
+		this.check(response);
+	},
+
+	check(response){
+
+		const answer = this.question.realName;
+
+		if(answer === response){
+
+			//call to render
+			view.render(view.result,'Correct!',{'class' :'correct'});
+			alert("Correct !");
+			this.score++;
+			//call to render
+			view.render(view.score,this.score);
+		}
+		else{
+
+			//call to render
+
+			view.render(view.result,`Wrong ! The correct answer was ${answer}`,{'class':'wrong'});
+			 alert(`Wrong! The correct answer is ${answer}`);
+
+		}
+	},
+
+	gameOver(){
+
+
+		//call to render 
+		view.render(view.info,`Game Over,You sccored ${this.score} point${this.score !== 1? 's':''}`);
+
+		
+	}
+
+
+
+};
+
+
+
+
+
+
+
+game.start(quiz);
